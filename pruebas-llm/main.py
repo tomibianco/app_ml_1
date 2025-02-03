2e783897860fd99caf8781a02f43be8a4c9c7a8b
tomibianco <tomasgbianco27@gmail.com>
2025-02-03 16:11:34 -0300

    updates

--- pruebas-llm/main.py
+++ pruebas-llm/main.py
@@ -7,10 +7,10 @@ df = pd.read_csv("/home/tomibianco/appml/pruebas-llm/libro.csv")
 # Cargar el modelo de DeepSeek desde Ollama
 llm = ChatOllama(model="deepseek-r1:8b")
 
-# Convertimos el dataframe en un string estructurado para el modelo
+# Convertimos el dataframe en string estructurado
 data_str = df.to_string(index=False)
 
-# Definimos la consulta
+# Definimos el prompt
 query = f"""
 Tengo los siguientes datos de ventas:
 
@@ -19,7 +19,7 @@ Tengo los siguientes datos de ventas:
 Dime cuantos trabajadores tienen más de 5 ventas
 """
 
-# Ejecutamos la consulta en DeepSeek
+# Ejecutamos el prompt en DeepSeek
 response = llm.invoke(query)
 
 # Mostramos la respuesta
